c2aa545cee639ad11777edb74baf7a9bfd487a57
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-03-16 20:52:10 +0400

    Added some more interactions
    Fixed a few small bugs.
    
    Added a go back function, setting left and right motor speed,
    going backward, left and right. Freddie seems to be moving nicely and w/o much cummulative error at 0.5speed.
    
    Also added resetCamera() call upon connection.

--- basicInteractions.py
+++ basicInteractions.py
@@ -3,12 +3,27 @@ import socket
 import time
 
 class BasicInteractionsWithFreddie:
+	# Forward, backward, left, right, stop commands
 	FWD = b'\xff\0\x01\0\xff'
+	BCK = b'\xff\0\x02\0\xff'
+	LFT = b'\xff\0\x03\0\xff'
+	RHT = b'\xff\0\x04\0\xff'
 	STP = b'\xff\0\0\0\xff'
+
+	# Set left and right motor speed prefix
+	LMS = b'\xff\x02\x01'
+	RMS = b'\xff\x02\x02' 
+
+	# Camera pan and tilt command prefix
 	PAN = b'\xff\x01\x07'
 	TLT = b'\xff\x01\x08'
+	
+	# For incremental camera changes
 	currTLT = 0
 	currPAN = 0
+
+	currLMS = 100
+	currRMS = 100
 
 	def __init__(self):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,11 +32,33 @@ class BasicInteractionsWithFreddie:
 		TCP_IP = '192.168.1.1'
 		TCP_PORT = 2001
 		self.s.connect((TCP_IP, TCP_PORT))
+		self.resetFreddiesCamera()
+
+	def setFreddiesLeftMotorSpeed(self, lms):
+		self.s.send(self.LMS + bytes([lms]) + b'\xff')
+
+	def setFreddiesRightMotorSpeed(self, rms):
+		self.s.send(self.RMS + bytes([rms]) + b'\xff')	
 
 	def sendFreddieForward(self, durationSeconds):
-		self.s.send(FWD)
+		self.s.send(self.FWD)
 		time.sleep(durationSeconds)
-		self.s.send(STOP)
+		self.s.send(self.STP)
+
+	def sendFreddieBackward(self, durationSeconds):
+		self.s.send(self.BCK)
+		time.sleep(durationSeconds)
+		self.s.send(self.STP)		
+
+	def turnFreddieLeft(self):
+		self.s.send(self.LFT)
+		time.sleep(0.5)
+		self.s.send(self.STP)
+
+	def turnFreddieRight (self):
+		self.s.send(self.RHT)
+		time.sleep(0.5)
+		self.s.send(self.STP)		
 
 	def panFreddiesCameraAbs(self, deg):
 		self.currPAN = deg
@@ -47,8 +84,13 @@ class BasicInteractionsWithFreddie:
 	def closeConnectionToFreddie(self):
 		self.s.close()
 
-
 freddie = BasicInteractionsWithFreddie()
 freddie.connectToFreddie()
-freddie.resetFreddiesCamera()
+freddie.setFreddiesLeftMotorSpeed(50)
+freddie.setFreddiesRightMotorSpeed(50)
+
+freddie.sendFreddieBackward(1)
+freddie.sendFreddieForward(0.25)
+freddie.turnFreddieLeft()
+freddie.sendFreddieForward(0.5)
 freddie.closeConnectionToFreddie()

81af4e0ba747585498f15b447c3ea928d31f6220
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-03-30 23:55:32 +0400

    Code cleanup
    Performed code cleanup. LineDetector is a class, comments were added and everything was tested with a video stream.

--- edgeDetection.py
+++ edgeDetection.py
@@ -1,92 +1,95 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-import glob
 from random import randint
 from math import sqrt
 
-def auto_canny(image, sigma=0.33):
-	median = np.median(image)
-	lower = int(max(0, (1.0-sigma)*median))
-	upper = int(min(255, (1.0 + sigma)*median))
-	return cv2.Canny(image, lower, upper)
+class LineDetector:
 
-def showLines(img, lines):
-	randColor=[0, 0, 0]
-	for l in lines:
-		for i in range (3):
-			randColor[i] = randint(0, 255)
+	### Canny detector, invariant to a change in overall image brightness.
+	### Requires no pixel boundary params specifications
+	def autoCanny(image, sigma=0.33):
+		median = np.median(image)
+		lower = int(max(0, (1.0-sigma)*median))
+		upper = int(min(255, (1.0 + sigma)*median))
+		return cv2.Canny(image, lower, upper)
 
-		x1 = l[0]
-		y1 = l[1]
-		x2 = l[2]
-		y2 = l[3]
-		cv2.line(img,(x1,y1),(x2,y2),tuple(randColor),3)
+	### Displays a list of lines onto an image using random colors.
+	def showLines(img, lines):
+		randColor=[0, 0, 0]
+		for l in lines:
+			for i in range (3):
+				randColor[i] = randint(0, 255)
 
-def houghLines(img, edged_frame):
-	h = (img.shape)[0]
-	w = (img.shape)[1]
-	LINE_LENGTH_THRESHOLD = int(h/3)
-	RO_ACCURACY = 3
-	THETA_ACCURACY = np.pi/180
-	# can be increased to detect more lines
-	MAX_LINE_GAP = 10
-	N_LONGEST_LINES = 2
+			x1 = l[0]
+			y1 = l[1]
+			x2 = l[2]
+			y2 = l[3]
+			cv2.line(img,(x1,y1),(x2,y2),tuple(randColor),3)
 
-	longestLines = {}
-	for i in range(N_LONGEST_LINES):
-		arr = [0, 0, 0, 0]
-		longestLines[i] = arr
+	### Inputs: edges, usually output from a Canny detector, 
+	### 		nLongestLines the caller is trying to identify,
+	###			all lineLengthThreshold pixels or larger. 
+	### Method: HoughLinesP identifies all such lines,
+	###			we dynamically keep track of the longest ones in a set.
+	###	Output:	a list of nLongestLines longest lines identified in the image.
+	def getLongestLines(edgedImage, nLongestLines, lineLengthThreshold): 
+		
+		RO_ACCURACY = 3
+		THETA_ACCURACY = np.pi/180
+		
+		# can be increased to detect more lines
+		MAX_LINE_GAP = 10
+		
+		longestLines = {}
+		for i in range(nLongestLines):
+			arr = [0, 0, 0, 0]
+			longestLines[i] = arr
 
-	#edged_frame = edged_frame/255
-	allLines = cv2.HoughLinesP(edged_frame, RO_ACCURACY, THETA_ACCURACY, 
-		threshold=LINE_LENGTH_THRESHOLD, 
-		minLineLength=LINE_LENGTH_THRESHOLD,
-		maxLineGap=MAX_LINE_GAP)
+		allLines = cv2.HoughLinesP(edgedImage, RO_ACCURACY, THETA_ACCURACY, 
+			threshold=lineLengthThreshold, 
+			minLineLength=lineLengthThreshold,
+			maxLineGap=MAX_LINE_GAP)
 
-	if allLines is None:
-		print('No lines')
-		return
+		if allLines is None:
+			return
 
-	for l in allLines:
-		for line in l:
-			x1 = line[0]
-			y1 = line[1]
-			x2 = line[2]
-			y2 = line[3]
-			lineLength = sqrt((y2-y1)**2 + (x2-x1)**2)
+		for l in allLines:
+			for line in l:
+				x1 = line[0]
+				y1 = line[1]
+				x2 = line[2]
+				y2 = line[3]
+				lineLength = sqrt((y2-y1)**2 + (x2-x1)**2)
 
-			sortedLengths = sorted(longestLines.keys())
+				sortedLengths = sorted(longestLines.keys())
 
-			if (lineLength>sortedLengths[0]):
-				# It could happen that this length is already present
-				if lineLength in longestLines:
-					lineLength += 0.00001*randint(1,10)
+				if (lineLength>sortedLengths[0]):
+					# It could happen that this length is already present
+					if lineLength in longestLines:
+						lineLength += 0.00001*randint(1,10)
 
-				del longestLines[sortedLengths[0]]
-				longestLines[lineLength] = list([x1,y1,x2,y2])
+					del longestLines[sortedLengths[0]]
+					longestLines[lineLength] = list([x1,y1,x2,y2])
 
-	showLines(img, list(longestLines.values()))
+		return list(longestLines.values())
 				
+# For testing
 cap=cv2.VideoCapture('http://192.168.1.1:8080/?action=stream')
 
 while True:
 	ret,frame=cap.read()
-	cv2.imshow("test-h264", frame)
+	cv2.imshow("frame", frame)
+	
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	edged_frame = auto_canny(gray)
-	houghLines(frame, edged_frame)
+	edgedFrame = LineDetector.autoCanny(gray)
+	lines = LineDetector.getLongestLines(edgedFrame, 
+		nLongestLines=1, lineLengthThreshold=int((edgedFrame.shape)[0]/3))
+	if lines is not None:
+		LineDetector.showLines(frame, lines)
+
 	cv2.imshow('Lines', frame)
 	cv2.waitKey(1)
 
 cap.release()
 cv2.destroyAllWindows()
-
-#img = cv2.imread('tstPics/tst.jpg')
-#cv2.imshow('original', img)
-#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#edged_frame = auto_canny(gray)
-#houghLines(img, edged_frame)
-#cv2.imshow('Lines', img)
-
-#cv2.waitKey(0)

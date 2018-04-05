7f8d6301be59aaa8cc0f4ce57313b8ec2ec3c7a0
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-04-05 11:43:21 +0400

    Code cleanup
    Tested with shoelace following. Though it is not the most robust piece of software out there,
    it worked. The lines were detected nicely, and the reduced frame rate helped the overall system.
    The final output was horizontal distance to center of the frame, which was printed to terminal
    only to be piped to the control code.

--- edgeDetection.py
+++ edgeDetection.py
@@ -4,50 +4,42 @@ from matplotlib import pyplot as plt
 from random import randint
 from math import sqrt, atan2, pi
 from sys import stdout
-from basicInteractions import BasicInteractionsWithFreddie
+from time import sleep
 
 class LineDetector:
 
-	def __init__():
-		freddie = BasicInteractionsWithFreddie()
-		freddie.connectToFreddie()
-		freddie.tiltFreddiesCameraAbs(40)
-		freddie.panFreddiesCameraAbs(90)
-		freddie.closeConnectionToFreddie()
-	### Canny detector, invariant to a change in overall image brightness.
-	### Requires no pixel boundary params specifications
+	"""" Canny detector, invariant to a change in overall image brightness.
+		Requires no pixel boundary params specifications """
 	def autoCanny(image, sigma=0.33):
 		median = np.median(image)
 		lower = int(max(0, (1.0-sigma)*median))
 		upper = int(min(255, (1.0 + sigma)*median))
 		return cv2.Canny(image, lower, upper)
 
-	### Displays a list of lines onto an image using random colors.
+	### Display a list of lines onto an image using random colors.
 	def showLines(img, lines):
 		randColor=[0, 0, 0]
 		for l in lines:
 			for i in range (3):
 				randColor[i] = randint(0, 255)
 
-			x1 = l[0]
-			y1 = l[1]
-			x2 = l[2]
-			y2 = l[3]
+			x1, y1, x2, y2 = l
 			cv2.line(img,(x1,y1),(x2,y2),tuple(randColor),3)
 
-	### Inputs: edges, usually output from a Canny detector, 
-	### 		nLongestLines the caller is trying to identify,
-	###			all lineLengthThreshold pixels or larger. 
-	### Method: HoughLinesP identifies all such lines,
-	###			we dynamically keep track of the longest ones in a set.
-	###	Output:	a list of nLongestLines longest lines identified in the image.
+	""" 
+		Inputs: edges, usually output from a Canny detector, 
+	 	nLongestLines the caller is trying to identify,
+		all lineLengthThreshold pixels or larger. 
+		Method: HoughLinesP identifies all such lines,
+		we dynamically keep track of the longest ones in a set.
+		Output:	a list of nLongestLines identified in the image.
+	"""
 	def getLongestLines(edgedImage, nLongestLines, lineLengthThreshold): 
 		
 		RO_ACCURACY = 3
 		THETA_ACCURACY = np.pi/180
-		
 		# can be increased to detect more lines
-		MAX_LINE_GAP = 20
+		MAX_LINE_GAP = 15
 		
 		longestLines = {}
 		for i in range(nLongestLines):
@@ -64,14 +56,12 @@ class LineDetector:
 
 		for l in allLines:
 			for line in l:
-				x1 = line[0]
-				y1 = line[1]
-				x2 = line[2]
-				y2 = line[3]
+				x1, y1, x2, y2 = line
 				lineLength = sqrt((y2-y1)**2 + (x2-x1)**2)
 
 				sortedLengths = sorted(longestLines.keys())
 
+				# Replace the shortest so far in the set
 				if (lineLength>sortedLengths[0]):
 					# It could happen that this length is already present
 					if lineLength in longestLines:
@@ -82,9 +72,21 @@ class LineDetector:
 
 		return list(longestLines.values())
 	
-	### Inputs: expects numpy or other array-like structures where 
-	###			subtraction is defined
-	###	Output: angle in degrees to the nearest point on the line
+	""" 
+		Inputs: numpy or other array-like structures where 
+				subtraction is defined
+		Output: distance in pixels to the farther endpoint on the segment.
+	"""
+	def distanceToFartherEndpointOnSegment(point, segment0, segment1, frameWidth):
+		fartherEndpoint=segment1 if segment1[1] - segment0[1] > 0 else segment0
+		return int(fartherEndpoint[0]-frameWidth/2)
+	
+	""" 
+		Not being used anywhere for now.
+		Inputs: numpy or other array-like structures where 
+		subtraction is defined
+		Output: angle in degrees to the nearest point on the line 
+	"""
 	def angleFromPointToLineSegment(point, segment0, segment1):
 		v = segment1 - segment0
 		w = point - segment0
@@ -100,40 +102,39 @@ class LineDetector:
 		directionToPoint = pointOnSegment-point
 		return atan2(directionToPoint[1], directionToPoint[0])*180/pi
 
-	### Inputs: expects numpy or other array-like structures where 
-	###			subtraction is defined
-	###	Output: angle in degrees to the farthest endpoint on the segment.
-	###			The angle is relative to line x=point[0], oriented N
-	def angleToFartherEndpointOnSegment(point, segment0, segment1):
-		fartherEndpoint=segment1 if segment1[1] - segment0[1] > 0 else segment0
-		angle = pi/2 + atan2(point[1]-fartherEndpoint[1], point[0]-fartherEndpoint[0])
-		#return angle*180/pi
-		return fartherEndpoint[0]-320
-
-
 def main():
 
 	cap=cv2.VideoCapture('http://192.168.1.1:8080/?action=stream')
-	lastOutput=0
+	ret,frame=cap.read()
+	LINE_LENGTH_THRESHOLD = int((frame.shape)[0]/5)
+	FRAME_WIDTH = frame.shape[1]
+	
+	frameCount = 0
 	while True:
 		ret,frame=cap.read()
+		frameCount+=1
+		# Reducing FPS by 5 for better processing
+		if (frameCount%5 != 0):
+			continue
 		
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		edgedFrame = LineDetector.autoCanny(gray)
-		lines = LineDetector.getLongestLines(edgedFrame, 
-			nLongestLines=1, lineLengthThreshold=int((edgedFrame.shape)[0]/10))
-		if lines is not None:
-			lastOutput = LineDetector.angleToFartherEndpointOnSegment \
-				(np.array([edgedFrame.shape[1]/2, 0]), \
-					np.array(lines)[0, :2], np.array(lines)[0, 2:])
-			LineDetector.showLines(frame, lines)
-
-		print(lastOutput)
-		stdout.flush()
+		longestLine = LineDetector.getLongestLines(edgedFrame, 
+			nLongestLines=1, lineLengthThreshold=LINE_LENGTH_THRESHOLD)
+		if longestLine is not None:
+			horizontalDistanceFromCenter = \
+				LineDetector.distanceToFartherEndpointOnSegment(\
+					np.array([edgedFrame.shape[1]/2, 0]), \
+					np.array(longestLine)[0, :2], \
+					np.array(longestLine)[0, 2:], FRAME_WIDTH)
 			
-		cv2.imshow('Lines', frame)
-		#cv2.imshow("frame", edgedFrame)
+			LineDetector.showLines(frame, longestLine)
 
+			print(horizontalDistanceFromCenter)
+			stdout.flush()
+		
+		frameCount=0
+		cv2.imshow('Lines', frame)
 		cv2.waitKey(1)
 
 	cap.release()

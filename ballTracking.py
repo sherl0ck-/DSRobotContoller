a775127811abfe8f335ff545e36601a33d0388a8
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-04-18 17:26:50 +0400

    Deleted some comments

--- ballTracking.py
+++ ballTracking.py
@@ -10,9 +10,6 @@ from sys import stdout
 #  - Stop when we approach the ball on the robot
 #  - Note where the ball dissapears (R/L)
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
 redLower = (0, 96, 31)
 redUpper = (6, 255, 255)
 
@@ -20,7 +17,8 @@ nFrames = 0
 camera=cv2.VideoCapture('http://192.168.1.1:8080/?action=stream')
 (grabbed, frame) = camera.read()
 halfFrameWidth = int(frame.shape[1]/2)
-print(int(halfFrameWidth))
+
+print(int(halfFrameWidth)) # For protocol purpose
 stdout.flush()
 
 while True:
@@ -29,30 +27,25 @@ while True:
     nFrames=(nFrames+1)%5
     if (nFrames!=0):
         continue
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
+    
     if not grabbed:
         break
  
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    # frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
  
-    # construct a mask for the color "green", then perform
+    # construct a mask for the color, then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
     mask = cv2.inRange(hsv, redLower, redUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
-        # find contours in the mask and initialize the current
+    
+    # find contours in the mask and initialize the current
     # (x, y) center of the ball
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
  
-    # only proceed if at least one contour was found
     if len(cnts) > 0:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
@@ -62,11 +55,8 @@ while True:
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
  
-        # only proceed if the radius meets a minimum size
         if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            
+            # draw the circle and centroid on the frame,            
             cv2.circle(frame, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)

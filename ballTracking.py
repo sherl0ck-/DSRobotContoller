bba73fabc3dde3ddc02f95e3f6a1a8ab42925a14
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-04-25 15:25:58 +0400

    Added different colors for experimenting
    Using a yellow-taped House ball now, so adjusted the color thresholds accordingly.

--- ballTracking.py
+++ ballTracking.py
@@ -13,11 +13,20 @@ from sys import stdout
 redLower = (0, 96, 31)
 redUpper = (6, 255, 255)
 
+blueLower = (104, 54, 0)
+blueUpper = (143, 183, 255)
+
+yellowLower = (25, 73, 104)
+yellowUpper = (62, 255, 255)
+
 nFrames = 0
 camera=cv2.VideoCapture('http://192.168.1.1:8080/?action=stream')
 (grabbed, frame) = camera.read()
 halfFrameWidth = int(frame.shape[1]/2)
 halfFrameHeight = int(frame.shape[0]/2)
+MIN_BALL_RADIUS = 10
+UNDER_OUR_NOSE=450
+IN_FRONT_OF_US=0.75*halfFrameHeight
 
 print(int(halfFrameWidth)) # For protocol purpose
 stdout.flush()
@@ -37,7 +46,7 @@ while True:
     # construct a mask for the color, then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
-    mask = cv2.inRange(hsv, redLower, redUpper)
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
@@ -56,17 +65,17 @@ while True:
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-        if radius > 10:
+        if radius > MIN_BALL_RADIUS:
             # draw the circle and centroid on the frame,
             cv2.circle(frame, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
             # Ball is below the camera, which means we arrived at the destination
-            if int(y)>450:
+            if int(y)>380:
                 print(-halfFrameWidth)
                 break
-            if int(radius) > 0.75*halfFrameHeight:
+            if int(radius) > IN_FRONT_OF_US:
                 print(-halfFrameWidth)
             else:
                 print(int(x) - halfFrameWidth)

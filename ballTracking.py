162469b009b6f15e579a97efbaa0c708b004435f
sherl0ck- <jovan.z.jovancevic@gmail.com>
2018-05-02 12:43:28 +0400

    Brought back isStopConditionMet

--- ballTracking.py
+++ ballTracking.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque   
 import numpy as np
 import argparse
 import imutils
@@ -7,58 +7,64 @@ from sys import stdout, stderr
 import os
 
 problems = ['atob', 'leader', 'trajectory']
+def isStopConditionMet(relevantParam, problem):
+	if problem in problems:
+		if problem == 'atob' or problem=='trajectory':
+			return relevantParam > 380
+		elif problem == 'leader':
+			return relevantParam > IN_FRONT_OF_US
 
 def getBallDirection(lab, colorRange):
-    colorLower, colorUpper = colorRange
-    # construct a mask for the color, then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(lab, colorLower, colorUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+	colorLower, colorUpper = colorRange
+	# construct a mask for the color, then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(lab, colorLower, colorUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
 
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-        if radius > MIN_BALL_RADIUS:
-            # draw the circle and centroid on the frame,
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		if radius > MIN_BALL_RADIUS:
+			# draw the circle and centroid on the frame,
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-            if problem == 'leader':
-                relevantParam = int(radius)
-            elif problem == 'atob' or problem=='trajectory':
-                relevantParam = int(y)
+			if problem == 'leader':
+				relevantParam = int(radius)
+			elif problem == 'atob' or problem=='trajectory':
+				relevantParam = int(y)
 
-            else:
-                relevantParam = None
+			else:
+				relevantParam = None
 
-            radius = int(radius)
-            print(-halfFrameWidth, radius) if isStopConditionMet(relevantParam, problem) \
-                else print(int(x)-halfFrameWidth, radius)
+			radius = int(radius)
+			print(-halfFrameWidth, radius) if isStopConditionMet(relevantParam, problem) \
+				else print(int(x)-halfFrameWidth, radius)
 
-            stdout.flush()
+			stdout.flush()
 
-        else:
-            print(halfFrameWidth, 0)
-            stdout.flush()
+		else:
+			print(halfFrameWidth, 0)
+			stdout.flush()
 
-    else:
-        print(halfFrameWidth, 0)
-        stdout.flush()
+	else:
+		print(halfFrameWidth, 0)
+		stdout.flush()
 
 redLower = (0, 96, 31)
 redUpper = (6, 255, 255)
@@ -81,7 +87,7 @@ orangeBalloon = [(0, 147, 145), (255, 171, 184)]
 blueBalloon = [(0, 91, 68), (255, 170, 118)]
 pinkBalloon = [(0, 165, 126), (255, 184, 146)]
 
-colorsToFollow = [yellowBalloon, redBalloon, orangeBalloon, blueBalloon, pinkBalloon]
+colorsToFollow = [yellowBalloon, redBalloon, blueBalloon]#, blueBalloon, pinkBalloon]
 FIFO = 'mypipe'
 os.mkfifo(FIFO, 0o777)
 
@@ -99,36 +105,36 @@ stdout.flush()
 colorIdx = 0
 
 while True:
-    # grab the current frame
-    (grabbed, frame) = camera.read()
-    #nFrames = (nFrames+1) % 1
-    #if (nFrames!=0):
-    #    continue
-    if not grabbed:
-        break
+	# grab the current frame
+	(grabbed, frame) = camera.read()
+	#nFrames = (nFrames+1) % 1
+	#if (nFrames!=0):
+	#    continue
+	if not grabbed:
+		break
 
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-    try:
-        p = os.open(FIFO, os.O_RDONLY | os.O_NONBLOCK)
-        input = os.read(p, 2)
-    except:
-        pass
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+	try:
+		p = os.open(FIFO, os.O_RDONLY | os.O_NONBLOCK)
+		input = os.read(p, 2)
+	except:
+		pass
 
-    if input:
-        colorIdx+=1
+	if input:
+		colorIdx+=1
 
-    getBallDirection(lab, colorsToFollow[colorIdx])
-    os.close(p)
-    # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+	getBallDirection(lab, colorsToFollow[colorIdx])
+	os.close(p)
+	# show the frame to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
 
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        print(-halfFrameWidth, 0)
-        stdout.flush()
-        break
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		print(-halfFrameWidth, 0)
+		stdout.flush()
+		break
 os.unlink(p)
 # cleanup the camera and close any open windows
 camera.release()
